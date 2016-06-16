91b5833651bbb225a89d8facb33d91e41c82ba49
Michał Dolata <dev@dolata.me>
2016-06-16 09:50:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+/**
+ * Created by preb on 07.05.16.
+ */
+document.getElementById("mobile-menu-button").onclick = function () {
+    document.getElementById("mobile-menu-wrapper").classList.toggle("active_menu");
+};
